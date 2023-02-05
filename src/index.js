c047b83524484789d9db6312236f6596f304fbb9
HuaCatLuong <hualuong23596@gmail.com>
2023-02-05 16:25:58 +0700

    a

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {AuthContextProvider} from "./store/auth-context";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <AuthContextProvider>
                 <App/>
             </AuthContextProvider>
