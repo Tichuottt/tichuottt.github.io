106f2c944587ec852ebf79b489522d546f2d0b45
HuaCatLuong <hualuong23596@gmail.com>
2023-02-05 16:04:42 +0700

    a

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import {BrowserRouter} from "react-router-dom";
 import App from './App';
 import {AuthContextProvider} from "./store/auth-context";
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
