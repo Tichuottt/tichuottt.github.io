5da712aafbd8c2f936cb657fbbb672fae1196be3
HuaCatLuong <hualuong23596@gmail.com>
2023-02-23 22:04:41 +0700

    add firebase hosting

--- src/index.js
+++ src/index.js
@@ -13,11 +13,11 @@ import {AuthContextProvider} from "./store/auth-context";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <AuthContextProvider>
                 <App/>
             </AuthContextProvider>
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>
 );
 
